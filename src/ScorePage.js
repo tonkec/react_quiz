65826284a9c856764571b6a32a1837c356b149bc
Antonija Simic <antonija1023@gmail.com>
2022-06-21 12:31:28 +0200

    remove esapcer; set html dangerouslly

--- src/ScorePage.js
+++ src/ScorePage.js
@@ -1,6 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { heading1 } from "./Typography";
-import {unescape} from 'html-escaper';
 
 const ScorePage = () => {
     let navigate = useNavigate();
@@ -11,12 +10,19 @@ const ScorePage = () => {
     const navigateToHomePage = () => {
         navigate("/")
     }
+
+    const createMarkup = (question) => {
+        return {__html: question};
+      }
     return (
         <>
          <h1 className={`${heading1} font-bold mb-10`}>You scored {score.length} / {questions.length}</h1>
          <ul className="text-left">
             {questions.slice(0, 5).map((question, i) => {
-                return <li className="text-ternary" key={i}> <ListIemMark question={question} /> {unescape(question.question.question)}</li>
+                return <li className="text-ternary" key={i}>
+                    <ListIemMark question={question} /> 
+                    <span dangerouslySetInnerHTML={createMarkup(question.question.question)}></span>
+                </li>
             })}
          </ul>
          <button className="mt-20" onClick={navigateToHomePage}>Play again?</button>
@@ -25,7 +31,7 @@ const ScorePage = () => {
 }
 
 const ListIemMark = ({question}) => (
-    <span className="text-2xl text-ternary">{question.question.correct_answer.toLowerCase() === question.answer ? "+" : "-"}</span>
+    <span className="text-2xl text-ternary mr-2">{question.question.correct_answer.toLowerCase() === question.answer ? "+" : "-"}</span>
 )
 
 export default ScorePage;
