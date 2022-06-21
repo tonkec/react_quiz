f021ff3306f0330af4f3f71df2aac8995659c37a
Antonija Simic <antonija1023@gmail.com>
2022-06-21 12:43:17 +0200

    add implict returns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/routes";
 import LayoutComponent from './components/helpers/LayouComponent';
-function App() {
-  return <LayoutComponent>
+const App = () => (
+   <LayoutComponent>
     <Router>
       <Routes />
     </Router>
   </LayoutComponent>
-    
-}
-
+)
 export default App;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,16 +3,14 @@ import QuestionPage from "./../components/QuestionPage";
 import QuestionPageLayout from "./../components/QuestionPageLayout";
 import ScorePage from "./../components/ScorePage";
 import HomePage from "./../components/Homepage"
-const QuizRoutes = () => {
-    return (
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="questions" element={<QuestionPageLayout />}>
-            <Route path=":id" element={<QuestionPage />} />
-          </Route>
-          <Route path="score" element={<ScorePage />} />
-      </Routes>
-    );
-  };
+const QuizRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="questions" element={<QuestionPageLayout />}>
+      <Route path=":id" element={<QuestionPage />} />
+    </Route>
+    <Route path="score" element={<ScorePage />} />
+  </Routes>
+);
   
   export default QuizRoutes;
