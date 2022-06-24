e0e1f5725c7ce60cb37f51e96847f63b85e0ccdd
Antonija Simic <antonija1023@gmail.com>
2022-06-24 17:46:05 +0200

    move api uri to env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const BASE_URI = process.env.REACT_APP_API_URL;
 export const getAllQuestionsFromApi = async () => {    
     try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean`)
+        const response = await axios.get(`${BASE_URI}?amount=10&difficulty=hard&type=boolean`)
         const questions = await response.data;
         if ("results" in questions) {
             const { results } = questions;
