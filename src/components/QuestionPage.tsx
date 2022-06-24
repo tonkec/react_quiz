b533c49c919834e0640a56b30c2a193f4cbab72b
Antonija Simic <antonija1023@gmail.com>
2022-06-24 16:26:18 +0200

    add types to QuestionPage

--- src/components/QuestionPage.tsx
+++ src/components/QuestionPage.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import {getAllQuestionsFromApi} from "../utils/api";
-import {useEffect, useState, useCallback} from "react";
+import {useEffect, useState, useCallback, MouseEvent } from "react";
 import {heading1, heading2, button} from "./helpers/Typography";
-
+import {Question} from "../model/Question"
 function QuestionPage() {
   let { id } = useParams();
-  let nextQuestionId = id;
+  let nextQuestionId = Number(id);
 
   let navigate = useNavigate();
-  const [allQuestions, setAllQuestions] = useState(null);
+  const [allQuestions, setAllQuestions] = useState<Question[] | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [answeredQuestions, setAnsweredQuestions] = useState([])  
+  const [answeredQuestions, setAnsweredQuestions] = useState<Question[] | any>([])  
   const currentQuestion = allQuestions ? allQuestions[Number(id) - 1] : null;
 
   const getAllQuestions = useCallback(async () => {
@@ -20,22 +19,27 @@ function QuestionPage() {
     setIsLoaded(true);
   }, [setIsLoaded, setAllQuestions])
 
-  const setQuestionsToState = (userValue) => {
-    setAnsweredQuestions((prevValue) => {
+  const setQuestionsToState = (userValue: any) => {
+    setAnsweredQuestions((prevValue: any) => { 
       return [...prevValue, {question: currentQuestion, answer: userValue}]
     })
   }
 
-  const navigateToNextQuestion = (e) => {
+  const navigateToNextQuestion = (event:MouseEvent<HTMLButtonElement>) => {
     nextQuestionId = Number(id) + 1;
-    setQuestionsToState(e.target.value);
-    if (Number(nextQuestionId) <= allQuestions.length + 1) {
-      navigate(`/questions/${Number(nextQuestionId)}`);
+    let button = event.target as HTMLButtonElement;
+    setQuestionsToState(button.value);
+    if (allQuestions) {
+      if (Number(nextQuestionId) <= allQuestions.length + 1) {
+        navigate(`/questions/${Number(nextQuestionId)}`);
+      }
     }
   }
 
   const createMarkup = () => {
+   if (currentQuestion) {
     return {__html: currentQuestion.question};
+   }
   }
 
   useEffect(() => {
@@ -55,14 +59,14 @@ function QuestionPage() {
        {isLoaded ? <>
         {currentQuestion && <h1 className={`${heading1} font-bold`}> {currentQuestion.category}</h1>}
        <div>
-       <div className='border-2 border-black p-2 mb-2'>
-          {currentQuestion && <h2 className={`${heading2} font-normal break-all`} dangerouslySetInnerHTML={createMarkup()}></h2>}
+        <div className='border-2 border-black p-2 mb-2'>
+            {currentQuestion && <h2 className={`${heading2} font-normal break-all`} dangerouslySetInnerHTML={createMarkup()}></h2>}
+          </div>
+          {allQuestions && <h2 className={`${heading2} font-normal`}> {`${nextQuestionId} of ${allQuestions.length}`}</h2>}
         </div>
-        {allQuestions && <h2 className={`${heading2} font-normal`}> {`${nextQuestionId} of ${allQuestions.length}`}</h2>}
-       </div>
         <div className='flex justify-between'>
-          <button className={button} value={true} onClick={navigateToNextQuestion}>True</button>
-          <button className={button} value={false} onClick={navigateToNextQuestion}>False</button>
+          <button className={button} value="true" onClick={navigateToNextQuestion}>True</button>
+          <button className={button} value="false" onClick={navigateToNextQuestion}>False</button>
         </div>
        </> : <h1>Loading...</h1>}
     </>
---
+++ src/model/Question.ts
@@ -0,0 +1,8 @@
+export interface Question {
+    category: string;
+    type: string;
+    difficulty: string;
+    correct_answer: string;
+    incorrect_answers: String[]
+    question: string
+ }
