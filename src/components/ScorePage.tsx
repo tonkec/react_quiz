434211206711afa0201c4df219dc1a679b6870ed
Antonija Simic <antonija1023@gmail.com>
2022-06-24 16:51:15 +0200

    add types to ScorePage

--- src/components/ScorePage.tsx
+++ src/components/ScorePage.tsx
@@ -1,18 +1,26 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { heading1 } from "./helpers/Typography";
+import {Question} from "../model/Question"
+
+interface Questions {
+    question: Question;
+    answer: String;
+}
+
+type LocationState = { questions: Questions[]; }
 
 const ScorePage = () => {
     let navigate = useNavigate();
-    const {state} = useLocation();
-    const {questions} = state;
+    const location = useLocation();
+    const {questions} = location.state as LocationState;
     const score = questions.filter((question) => question.question.correct_answer.toLowerCase() === question.answer);
 
     const navigateToHomePage = () => {
         navigate("/")
     }
 
-    const createMarkup = (question) => {
-        return {__html: question};
+    const createMarkup = (question: Question) => {
+        return {__html: question.question};
     }
     return (
         <>
@@ -21,7 +29,7 @@ const ScorePage = () => {
             {questions.slice(0, 5).map((question, i) => {
                 return <li className="text-ternary" key={i}>
                     <ListIemMark question={question} /> 
-                    <span dangerouslySetInnerHTML={createMarkup(question.question.question)}></span>
+                    <span dangerouslySetInnerHTML={createMarkup(question.question)}></span>
                 </li>
             })}
          </ul>
@@ -30,7 +38,7 @@ const ScorePage = () => {
     )
 }
 
-const ListIemMark = ({question}) => (
+const ListIemMark = ({question} : {question:any}) => (
     <span className="text-2xl text-ternary mr-2">{question.question.correct_answer.toLowerCase() === question.answer ? "+" : "-"}</span>
 )
 
